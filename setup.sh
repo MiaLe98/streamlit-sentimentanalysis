1c052481c13a4c626eea73563b465974dde97e6b
Mia Le <59309656+MiaLe98@users.noreply.github.com>
2020-11-10 15:22:47 +0200

    Delete setup.sh

--- setup.sh
+++
@@ -1,9 +0,0 @@
-mkdir -p ~/.streamlit/
-echo "[general]
-email = \"mia@ebbot.ai\"
-" > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-port = $PORT
-enableCORS = false
-" > ~/.streamlit/config.toml
